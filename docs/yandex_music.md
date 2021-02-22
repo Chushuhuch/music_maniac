722c7675de38ac0105b36d6d76731b0ae7a32a56
Sergey Bankevich <Sergey.Bankevich@gmail.com>
2021-02-22 22:20:02 +0300

    yandex docs + search track in yandex

---
+++ docs/yandex_music.md
@@ -0,0 +1,17 @@
+Looks like there is probably no official API.
+
+Unofficial API: https://yandex-music.readthedocs.io/ru/main/
+
+Official repo for player embedding https://github.com/yandex/audio-js/tree/master/spec.
+From the first glance it works with links to tracks. Thus link-to-song correspondence might not be totally fixed.
+Though, hardly it changes, at least not often.
+
+Some linux app integrating different services. But I can't find any API usage there, maybe it works with just web pages.
+https://github.com/tiliado/nuvolaplayer.
+Repo for yandex part (with no yandex page mentions): https://github.com/tiliado/nuvola-app-yandex-music.
+
+Very old repo with python API https://github.com/nvbn/python-yamusic/.
+
+Looks like a proper unofficial python API: https://github.com/MarshalX/yandex-music-api, https://pypi.org/project/yandex-music/, https://yandex-music.readthedocs.io/ru/main/
+
+TypeScript unofficial API: https://github.com/acherkashin/yandex-music-extension
--- pyBot/bot.py
+++ pyBot/bot.py
@@ -7,14 +7,18 @@ Usage:
 import abc
 import logging
 import re
+from collections import defaultdict
+from enum import Enum
 from http import HTTPStatus
 from logging import DEBUG
+from typing import Optional
 
 import requests
+import yandex_music
 
 from credentials import bot_token
 
-from telegram import Update
+from telegram import Update, ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 # Enable logging
@@ -35,21 +39,37 @@ def helpCommand(update: Update, context: CallbackContext) -> None:
                               'Then paste it here to get links to other services.')
 
 
+class MusicService(Enum):
+    APPLE = 1
+    YANDEX = 2
+
+
+class MediaType(Enum):
+    TRACK = 1
+
+
 class MediaObject:
 
-    def __init__(self, track: str = None, album: str = None, invalid: bool = False):
+    def __init__(self, track: str = None, album: str = None, origin: MusicService = None, link: str = None, mediaType: MediaType = MediaType.TRACK):
         self.track = track
         self.album = album
-        self.invalid = invalid
-
-
-MediaObject.INVALID_MEDIA_OBJECT = MediaObject(invalid = True)
+        self.origin = origin
+        self.link = defaultdict(str)
+        self.link[origin] = link
+        self.mediaType = mediaType
+        if not origin or not link:
+            logger.warning('Creating valid MediaObject without link or origin: {this}'.format(this = self))
 
 
 class MusicServiceManager(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
-    def parseMediaFromLink(self, link: str):
+    def parseMediaFromLink(self, link: str) -> Optional['MediaObject']:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def searchMediaByLink(self, mediaObject: MediaObject):
+        # Populates mediaObject with link in a corresponding music service
         raise NotImplementedError
 
 
@@ -59,31 +79,59 @@ class AppleMusicManager(MusicServiceManager):
     GET_INFO_BY_ID_REQUEST = 'https://itunes.apple.com/lookup?id={id}'
     IDS_REGEX = re.compile(r'(\d+)\?i=(\d+)&ls')
 
-    def parseMediaFromLink(self, link: str) -> MediaObject:
+    def parseMediaFromLink(self, link: str) -> Optional['MediaObject']:
         if not link.startswith(self.LINK_PREFIX):
-            return MediaObject.INVALID_MEDIA_OBJECT
+            return None
         parts = link[len(self.LINK_PREFIX):].split('/')
         if not len(parts) == 2:
-            return MediaObject.INVALID_MEDIA_OBJECT
+            return None
         _, ids = parts
         # ids == 'albumID?i=trackID&ls'
         match = re.match(self.IDS_REGEX, ids)
         if not match:
-            return MediaObject.INVALID_MEDIA_OBJECT
+            return None
 
         # collection_id = match.group(1)
         track_id = match.group(2)
 
         response = requests.get(self.GET_INFO_BY_ID_REQUEST.format(id = track_id))
         if response.status_code != HTTPStatus.OK:
-            return MediaObject.INVALID_MEDIA_OBJECT
+            return None
         response_json = response.json()
         if response_json['resultCount'] != 1 or 'trackName' not in response_json['results'][0]:
-            return MediaObject.INVALID_MEDIA_OBJECT
+            return None
         track_name = response_json['results'][0]['trackName']
         collection_name = response_json['results'][0]['collectionName']
 
-        return MediaObject(track = track_name, album = collection_name)
+        return MediaObject(track = track_name, album = collection_name, origin = MusicService.APPLE, link = link)
+
+    def searchMediaByLink(self, mediaObject: MediaObject):
+        raise NotImplementedError
+
+
+class YandexMusicManager(MusicServiceManager):
+
+    def __init__(self):
+        self.client = yandex_music.Client()
+
+    def parseMediaFromLink(self, link: str) -> Optional['MediaObject']:
+        raise NotImplementedError
+
+    def searchMediaByLink(self, mediaObject: MediaObject):
+        if mediaObject.mediaType != MediaType.TRACK:
+            raise NotImplementedError
+
+        search_result = self.client.search(text = '{album}{track}'.format(album = mediaObject.album + ' ' if mediaObject.album else '', track = mediaObject.track), nocorrect = True, type_ = 'track')
+        track_results = search_result.tracks
+        if not track_results:
+            logger.debug('Not found any tracks by the request')
+            return
+        tracks = track_results.results
+        matching_tracks = [track for track in tracks if (not mediaObject.album or len([album for album in track.albums if album.title == mediaObject.album]) > 0) and track.title == mediaObject.track]
+        if len(matching_tracks) != 1:
+            logger.debug('Found bad amount of tracks in Yandex.Music: {matching_tracks}'.format(matching_tracks = matching_tracks))
+        if matching_tracks:
+            mediaObject.link[MusicService.YANDEX] = 'https://music.yandex.ru/track/{track_id}'.format(track_id = matching_tracks[0].id)
 
 
 def send_reply(update: Update, text: str):
@@ -96,14 +144,18 @@ def processLink(update: Update, context: CallbackContext) -> None:
     logger.debug(text)
 
     apple_media_object = AppleMusicManager().parseMediaFromLink(text)
-    if apple_media_object == MediaObject.INVALID_MEDIA_OBJECT:
+    if not apple_media_object:
         send_reply(update, "Sorry, couldn't parse the link. We are working on this problem.")
         return
 
+    YandexMusicManager().searchMediaByLink(apple_media_object)
+
     track = apple_media_object.track
     album = apple_media_object.album
-    send_reply(update, 'This is a link to {track}{from_album}.'.
-                              format(track = track, from_album =' from album {album}'.format(album = album) if album is not None else ''))
+    send_reply(update, 'This is a link to {track}{from_album}. Yandex Music link is {yandex_link}'.
+                              format(track = track,
+                                     from_album =' from album {album}'.format(album = album) if album is not None else '',
+                                     yandex_link = apple_media_object.link[MusicService.YANDEX]))
 
 
 def main():
